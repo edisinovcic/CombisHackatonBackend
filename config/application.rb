b81a04cab918095dd6ef51632eb9f94750c1ab34
edi.sinovcic@gmail.com <edi.sinovcic@gmail.com>
2017-04-08 15:31:05 +0200

    Authentication added

--- config/application.rb
+++ config/application.rb
@@ -18,13 +18,20 @@ Bundler.require(*Rails.groups)
 
 module CombisHackatonBackend
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    class Application < Rails::Application
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+      # ...snip...
+
+      config.middleware.use Rack::Cors do
+        allow do
+          origins '*'
+          resource '*',
+                   :headers => :any,
+                   :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                   :methods => [:get, :post, :options, :delete, :put]
+        end
+      end
+    end
     config.api_only = true
   end
 end
