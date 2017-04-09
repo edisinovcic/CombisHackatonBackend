576ad8a8ef4348e081cc92435b1f1d7f33d4a87c
edi.sinovcic@gmail.com <edi.sinovcic@gmail.com>
2017-04-09 04:06:30 +0200

    Master updatež

--- db/migrate/20170409014339_create_reserve.rb
+++
@@ -1,6 +0,0 @@
-class CreateReserve < ActiveRecord::Migration[5.0]
-  def change
-    create_table :reserves do |t|
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408222147) do
+ActiveRecord::Schema.define(version: 20170409020315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reserves", force: :cascade do |t|
+    t.integer  "id_reserve"
+    t.integer  "id_user"
+    t.float    "amount"
+    t.date     "date"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "services", force: :cascade do |t|
     t.integer "service_id"
